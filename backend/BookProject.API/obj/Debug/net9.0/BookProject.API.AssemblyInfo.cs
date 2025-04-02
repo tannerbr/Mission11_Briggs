def67d776962cb9f6b64e219755d86f30c32f16e
tannerbr <tannerjbriggs@hotmail.com>
2025-04-01 19:28:15 -0600

    BookProject-Briggs-Frontend

--- backend/BookProject.API/obj/Debug/net9.0/BookProject.API.AssemblyInfo.cs
+++ backend/BookProject.API/obj/Debug/net9.0/BookProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d72f4e947d9c00101f96a74e6adc2a65b2648c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bfb063a9918d6ab03be45f0dd0df61137aa4d6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
